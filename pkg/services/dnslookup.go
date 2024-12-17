b503640a1eeadd2c574f72967126ec565c846657
grez-lucas <lucasgrez3010@gmail.com>
2024-12-17 13:53:48 -0300

    feat(dnslookup) Add net/http retrievable value fetching

--- pkg/services/dnslookup.go
+++ pkg/services/dnslookup.go
@@ -41,7 +41,7 @@ func (s *DNSLookupService) RunScan(targets []string) (*dom.DNSLookupEventResult,
 
 			result, err := performDNSLookup(target)
 			if err != nil {
-				s.Logger.Error("Error performing DNSLookup for target `%s`: %v", target, err)
+				s.Logger.Error("Error performing DNSLookup for target ", target, err)
 				mu.Lock()
 				errs = append(errs, err)
 				mu.Unlock()
@@ -63,26 +63,121 @@ func (s *DNSLookupService) RunScan(targets []string) (*dom.DNSLookupEventResult,
 }
 
 func performDNSLookup(domain string) (*dom.DNSLookupResult, error) {
+	// TODO: Handle errors
 	start := time.Now()
 
-	// Retrieve A records
-	ips, err := net.LookupIP(domain)
+	// Retrieve A and AAAA records
+	aRecords, aaaaRecords, err := lookupAandAAAA(domain)
 	if err != nil {
-		return nil, err
+		slog.Error(err.Error())
 	}
-	var aRecords []string
+
+	// Retrieve CNAME records
+	cnameRecords, err := lookupCNAME(domain)
+
+	// TXTRecords
+	txtRecords, err := lookupTXT(domain)
+
+	// Lookup NSRecords
+	nsRecords, err := lookupNS(domain)
+
+	// Retrieve MXRecords
+	mxRecords, err := lookupMX(domain)
+
+	duration := time.Since(start)
+
+	return &dom.DNSLookupResult{
+		Domain:         domain,
+		ARecords:       aRecords,
+		AAAARecords:    aaaaRecords,
+		CNAMERecords:   cnameRecords,
+		MXRecords:      mxRecords,
+		TXTRecords:     txtRecords,
+		NSRecords:      nsRecords,
+		LookupDuration: duration,
+		CreatedAt:      time.Now(),
+	}, nil
+
+}
+
+func lookupAandAAAA(domain string) ([]string, []string, error) {
+	var (
+		aRecords    []string
+		aaaaRecords []string
+	)
+
+	// Retrieve A and AAAA records
+	ips, err := net.LookupIP(domain)
+
+	if err != nil {
+		return aRecords, aaaaRecords, err
+	}
+
 	for _, ip := range ips {
 		if ip.To4() != nil {
 			aRecords = append(aRecords, ip.String())
 		}
+		if ip.To4() == nil {
+			aaaaRecords = append(aaaaRecords, ip.String())
+		}
 	}
 
-	duration := time.Since(start)
-	return &dom.DNSLookupResult{
-		Domain:         domain,
-		ARecords:       aRecords,
-		LookupDuration: duration,
-		CreatedAt:      time.Now(),
-	}, nil
+	return aRecords, aaaaRecords, err
+}
+
+func lookupCNAME(domain string) ([]string, error) {
+	var cnameRecords []string
+
+	cname, err := net.LookupCNAME(domain)
+	if err != nil {
+		return cnameRecords, fmt.Errorf("failed to get CNAME record: `%v`", err)
+	}
+	cnameRecords = append(cnameRecords, cname)
+	return cnameRecords, nil
+}
+
+func lookupTXT(domain string) ([]string, error) {
+	var txtRecords []string
+
+	records, err := net.LookupTXT(domain)
+	if err != nil {
+		return txtRecords, fmt.Errorf("failed to get TXT records: `%v`", err)
+	}
+	for _, record := range records {
+		txtRecords = append(txtRecords, record)
+	}
+	return txtRecords, nil
+
+}
+
+func lookupNS(domain string) ([]string, error) {
+	var nsRecords []string
+
+	records, err := net.LookupNS(domain)
+	if err != nil {
+		return nsRecords, fmt.Errorf("failed to get NS records: `%v`\n", err)
+	}
+	for _, ns := range records {
+		nsRecords = append(nsRecords, ns.Host)
+	}
+	return nsRecords, nil
+}
+
+func lookupMX(domain string) ([]dom.MailExchange, error) {
+	var mxRecords []dom.MailExchange
+
+	records, err := net.LookupMX(domain)
+	if err != nil {
+		return mxRecords, fmt.Errorf("failed to get MX records: `%v`\n", err)
+	}
+	for _, mx := range records {
+		r := dom.MailExchange{
+			Host:     mx.Host,
+			Priority: int(mx.Pref),
+		}
+		mxRecords = append(mxRecords, r)
+	}
+
+	return mxRecords, nil
 
 }
