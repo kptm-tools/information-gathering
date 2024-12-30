d35f6b0d487bb18d84e8a81f1ffb17309df8e6fc
grez-lucas <lucasgrez3010@gmail.com>
2024-12-30 17:52:12 -0300

    feat(events) Add event bus connection and setup subscription

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
 
+	cmmn "github.com/kptm-tools/common/common/events"
+	"github.com/kptm-tools/information-gathering/pkg/config"
+	"github.com/kptm-tools/information-gathering/pkg/events"
 	"github.com/kptm-tools/information-gathering/pkg/handlers"
 	"github.com/kptm-tools/information-gathering/pkg/services"
 )
 
 func main() {
 	fmt.Println("Hello information gathering!")
+	c := config.LoadConfig()
+
+	// Events
+	eventBus, err := cmmn.NewNatsEventBus(c.GetNatsConnStr())
+	if err != nil {
+		log.Fatalf("Error creating Event Bus: %s", err.Error())
+	}
 
 	// Services
 	whoIsService := services.NewWhoIsService()
@@ -20,16 +31,22 @@ func main() {
 	dnsLookupHandler := handlers.NewDNSLookupHandler(dnsLookupService)
 	harvesterHandler := handlers.NewHarvesterHandler(harvesterService)
 
-	if err := whoIsHandler.RunScan(); err != nil {
-		panic(err)
+	err = eventBus.Init(func() error {
+		if err := events.SubscribeToScanStarted(eventBus, whoIsHandler, dnsLookupHandler, harvesterHandler); err != nil {
+			return err
+		}
+		return nil
+	})
+	if err != nil {
+		log.Fatalf("Failed to initialize Event Bus: %s", err.Error())
 	}
 
-	if err := dnsLookupHandler.RunScan(); err != nil {
-		panic(err)
-	}
+	go forever()
+	select {}
 
-	if err := harvesterHandler.RunScan(); err != nil {
-		panic(err)
-	}
+}
 
+func forever() {
+	for {
+	}
 }
